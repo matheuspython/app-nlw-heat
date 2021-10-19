1af8588dc8329a44eeb4436faa2203a6fc344f01
matheus souza <ruivorobado@gmail.com>
2021-10-19 20:20:32 -0300

    rota que retorna ultimas 3 mensagens criada

--- node_heat/src/routes.ts
+++ node_heat/src/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express"; 
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { Get3LastMessagesController } from "./controllers/GetLast3MessageController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post('/authenticate', new AuthenticateUserController().handle)
 router.post('/messages', ensureAuthenticated ,new CreateMessageController().handle)
+router.get("/messages/last3", new Get3LastMessagesController().handle)
 
 export { router }
 
