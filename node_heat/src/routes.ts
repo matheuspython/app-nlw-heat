f068b8310ebc34551d71c5b8047aec81545c6c33
matheus souza <ruivorobado@gmail.com>
2021-10-19 20:44:10 -0300

    1 dia completo

--- node_heat/src/routes.ts
+++ node_heat/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { Get3LastMessagesController } from "./controllers/GetLast3MessageController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
@@ -9,6 +10,7 @@ const router = Router();
 router.post('/authenticate', new AuthenticateUserController().handle)
 router.post('/messages', ensureAuthenticated ,new CreateMessageController().handle)
 router.get("/messages/last3", new Get3LastMessagesController().handle)
+router.get('/profile', ensureAuthenticated,new ProfileUserController().handle)
 
 export { router }
 
