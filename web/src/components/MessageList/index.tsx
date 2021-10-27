2c64a4d8c4400693a6aa4925cf44f81c67bcd883
matheuspython <ruivorobado@gmail.com>
2021-10-26 23:46:39 -0300

    corrijindo erro avatar

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -59,7 +59,7 @@ const MessageList: React.FC = () => {
               <p className={styles.messageContent}>{message.text}</p>
               <div className={styles.messageUser}>
                 <div className={styles.userImage}>
-                  <img src={usuario.avatar_url} alt={message.user.name} />
+                  <img src={message.user.avatar_url} alt={message.user.name} />
                 </div>
                 <span>{message.user.name}</span>
 
